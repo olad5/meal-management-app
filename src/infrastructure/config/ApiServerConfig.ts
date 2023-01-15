e2f8351478f3da272b769332dd284a7aef719527
Francis Shonubi <shonubioladipo@gmail.com>
2023-01-15 10:52:29 +0100

    chore: change PORT env variable

--- src/infrastructure/config/ApiServerConfig.ts
+++ src/infrastructure/config/ApiServerConfig.ts
@@ -1,9 +1,7 @@
 import { get } from "env-var";
 
 export class ApiServerConfig {
-  public static readonly PORT: number = get("API_PORT")
-    .required()
-    .asPortNumber();
+  public static readonly PORT: number = get("PORT").required().asPortNumber();
 
   public static readonly LOG_ENABLE: boolean = get("API_LOG_ENABLE")
     .required()
